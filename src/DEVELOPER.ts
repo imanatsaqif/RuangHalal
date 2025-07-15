cba36f7c287ddb2667cd718ed69b3077fa133d12
Imana Tsaqif <iimtsaqif@gmail.com>
2025-07-15 15:30:03 +0700

    Menambah developer credit file di src

---
+++ src/DEVELOPER.ts
@@ -0,0 +1,22 @@
+/**
+ * Developer Credit File
+ * 
+ * Nama  : Imana Tsaqif Ariyadi
+ * GitHub: https://github.com/imanatsaqif
+ * Email : iimtsaqif@gmail.com
+ * 
+ * Project ini dikembangkan oleh Imana Tsaqif Ariyadi untuk RuangHalal.id
+ * Segala kontribusi, perbaikan, atau pertanyaan bisa mengacu pada repo GitHub di atas.
+ * 
+ * File ini tidak digunakan di UI agar tidak mengganggu pengalaman pengguna.
+ * Harap untuk tidak menghapus file ini sebagai bentuk penghargaan.
+ */
+
+const developerInfo = {
+  name: 'Imana Tsaqif Ariyadi',
+  github: 'https://github.com/imanatsaqif',
+  date: '2025-07',
+  project: 'Landing Page Sertifikasi Halal untuk RuangHalal.id',
+}
+
+export default developerInfo
