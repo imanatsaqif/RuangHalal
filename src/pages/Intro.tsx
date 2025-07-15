1a0e502d39b68ca0642c4eaf1295ec361e9a05a7
Imana Tsaqif <iimtsaqif@gmail.com>
2025-07-15 15:57:22 +0700

    Refaktor Intro, responsivitas, tombol language switcher

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,21 +1,36 @@
 import Formulir from '../components/Formulir'
 import FloatingButtons from '../components/FloatingButtons'
+import LanguageSwitcher from '../components/LanguageSwitcher'
+import { useHeaderHeight } from '../hooks/useHeaderHeight'
 
 function Intro() {
+  const headerHeight = useHeaderHeight()
+
   return (
-    <div className="min-h-screen flex bg-white relative">
+    <div
+      className="flex flex-col md:flex-row bg-white relative"
+      style={{ minHeight: `calc(100vh - ${headerHeight}px)` }}
+    >
+      {/* Language Switcher */}
+      <div className="absolute top-4 right-4 z-50">
+        <LanguageSwitcher />
+      </div>
+
       {/* Kolom kiri - Poster */}
-      <div className="w-1/2 h-screen bg-white flex items-center justify-end p-6 shadow-2xl">
+      <div className="w-full md:w-1/2 flex items-center justify-center p-6 ">
         <img
           src="/gambar-poster-halal.webp"
           alt="Poster Sertifikasi Halal"
-          className="h-full w-auto object-contain rounded-xl"
+          className="max-h-[80vh] w-auto object-contain rounded-xl mx-auto"
         />
       </div>
 
+
       {/* Kolom kanan - Formulir */}
-      <div className="w-1/2 bg-white flex items-center justify-start p-10">
-        <Formulir />
+      <div className="w-full md:w-1/2 flex items-center justify-center p-6 sm:p-10">
+        <div className="w-full">
+          <Formulir showSkip />
+        </div>
       </div>
 
       {/* Floating Buttons */}
