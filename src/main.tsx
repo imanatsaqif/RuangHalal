a64e1fb300ecc4d7ac889aa4a4da6328d64cf9be
Imana Tsaqif <iimtsaqif@gmail.com>
2025-07-15 15:58:05 +0700

    Refaktor main src, import meta data dan import library translate

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
+//src/main.tsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -11,3 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </React.StrictMode>,
 )
+
+if (import.meta.env.DEV) {
+  console.info('%cBuilt by Imana Tsaqif Ariyadi\n%cgithub.com/imanatsaqif', 'font-weight: bold; color: #128C7E', 'color: #555')
+}
