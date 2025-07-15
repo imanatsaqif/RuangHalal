b05b77e08f7bacf23d6414af0e8a70c3eb38aeb5
Imana Tsaqif <iimtsaqif@gmail.com>
2025-07-15 15:43:43 +0700

    Refaktor formSection, responsivitas, translate, hide heading di mobile

--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -1,15 +1,26 @@
-// src/components/FormSe.tsx
 import Formulir from './Formulir'
+import { useTranslation } from 'react-i18next'
+import { useHeaderHeight } from '../hooks/useHeaderHeight'
 
 const FormSection = () => {
+  const { t } = useTranslation()
+  const headerHeight = useHeaderHeight()
+
   return (
-    <section className="min-h-screen flex bg-white" id="formulir">
-      <div className="w-1/2 h-screen text-[#128C7E] flex items-center justify-center p-10">
-        <h2 className="text-4xl lg:text-5xl font-bold leading-snug max-w-xl text-center">
-          Lebih Mudah Dapatkan <br /> Sertifikat Halal Anda <br /> Bersama Kami
+    <section
+      id="formulir"
+      className="flex flex-col md:flex-row bg-white"
+      style={{ minHeight: `calc(100vh - ${headerHeight}px)` }}
+    >
+      {/* Kolom Kiri - Judul */}
+      <div className="w-full md:w-1/2 text-[#128C7E] items-center justify-center p-10 hidden md:flex">
+        <h2 className="text-4xl lg:text-5xl font-bold leading-snug max-w-xl text-center whitespace-pre-line">
+          {t('formulir.title')}
         </h2>
       </div>
-      <div className="w-1/2 bg-white flex items-center justify-center p-10">
+
+      {/* Kolom Kanan - Formulir */}
+      <div className="w-full md:w-1/2 flex items-center justify-center p-6 sm:p-10">
         <Formulir />
       </div>
     </section>
